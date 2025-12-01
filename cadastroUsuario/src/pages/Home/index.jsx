15eb6042c868667c36b09cb6330d10cbb70930f6
ChallengeOne-MAT <rm561090@fiap.com.br>
2025-12-01 18:54:31 -0300

    estilizando formulario

--- cadastroUsuario/src/pages/Home/index.jsx
+++ cadastroUsuario/src/pages/Home/index.jsx
@@ -1,29 +1,67 @@
-import { useState } from 'react'
+  import { useState } from 'react'
+  import Icon from '../../../../cadastroUsuario/public/lixeira.png'
+  import './estilo.css'
+  function Home() {
+    const Users = [
+  {
+    id: '1',
+    name: "Mateus",
+    age: 19,
+    email: "msmateuspereira@gmail.com"
+  },
+  {
+    id: '2',
+    name: "Ana",
+    age: 22,
+    email: "ana.silva@example.com"
+  },
+  {
+    id: '3',
+    name: "Carlos",
+    age: 25,
+    email: "carlos.martins@example.com"
+  },
+  {
+    id: '4',
+    name: "Beatriz",
+    age: 20,
+    email: "beatriz.lopes@example.com"
+  }
+];
 
-function Home() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className='container'>
-       <form>
-        <h1>Cadastro Usuario</h1>
-        <input name='name' type='text'/>
-        <input name='age' type='number'/>
-        <input name='email' type='email'/>
-<button type='button'>Enviar</button>
-     <div>
-      <div>
-        <p>Nome:</p>
-        <p>Idade:</p>
-        <p>E-mail:</p>
-      </div>
-     </div>
-       </form>
 
-      </div>
-    </>
-  )
-}
 
-export default Home
+    return (
+      <>
+ <div className='container'>
+        <form>
+         
+            <h1>Cadastro Usuario</h1>
+            <input name='name' type='text' placeholder='digite seu nome..'/>
+            <input name='age' type='number' placeholder='digite suas data de nascimento..' />
+            <input name='email' type='email' placeholder='digite seu melhor email..' />
+            <button type='button'>Enviar</button>
+        </form>
+          </div>
+
+{Users.map(user => (
+
+
+<div key={user.id} className='card'>
+    <div>
+      <p>Nome: <span>{user.name}</span>  </p>
+      <p>Idade: <span>{user.age}</span> </p>
+      <p>E-mail: <span>{user.email}</span> </p>
+    </div>
+    <button>
+      <img src={Icon} alt="img-icon" />
+    </button>
+  </div>
+))}
+
+      </>
+    )
+  }
+
+  export default Home
